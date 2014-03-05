14d4d30c66b4b3897a82607e892131547ba14ba1
Carl Schroedl <cschroedl@usgs.gov>
2014-03-05 13:48:58 -0600

    Extracted tier-specific into local_settings.py

--- nar_common/settings.py
+++ nar_common/settings.py
@@ -12,21 +12,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2e$%iu231_z_cqalvr7d*1xlbvxms40=ryo@u@(t0l0t^c9c_c'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = (
