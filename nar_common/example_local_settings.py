4dd3281b3fd7b5f589e61e6e5a9d61b6bc850caf
Carl Schroedl <cschroedl@usgs.gov>
2014-03-06 11:31:24 -0600

    Added example local settings

---
+++ nar_common/example_local_settings.py
@@ -0,0 +1,13 @@
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'ChangeMe'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
