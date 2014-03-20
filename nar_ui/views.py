9f4faa79fb16c83d440aee666b34a60d445292d7
Wernimont, Martin R <mwernimont@usgs.gov>
2014-03-20 11:04:03 -0500

    home page

--- nar_ui/views.py
+++ nar_ui/views.py
@@ -3,3 +3,6 @@ from django.views.generic.base import TemplateView
 class HomePageView(TemplateView):
     template_name = 'nar_ui/home.html'
     
+    
+class SiteSummaryReportView(TemplateView):
+    template_name = 'nar_ui/summary.html'
