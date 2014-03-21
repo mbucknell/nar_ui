28e23d6020af85a4ce18d278bbfca828e0fb2205
Carl Schroedl <cschroedl@usgs.gov>
2014-03-21 15:18:32 -0500

    Added mock map

---
+++ nar_common/static/nar_ui/map.js
@@ -0,0 +1,78 @@
+var map;
+(function() {
+	var options = {};
+	var WGS84_GOOGLE_MERCATOR = new OpenLayers.Projection('EPSG:900913'); 
+	var WGS84_GEOGRAPHIC = new OpenLayers.Projection('EPSG:4326');
+	options.projection = WGS84_GOOGLE_MERCATOR;
+	options.controls = [
+        new OpenLayers.Control.Navigation(),
+        new OpenLayers.Control.MousePosition({
+            prefix: 'POS: '
+        }),
+        new OpenLayers.Control.ScaleLine({
+            geodesic: true
+        }),
+        new OpenLayers.Control.LayerSwitcher({
+            roundedCorner: true
+        }),
+        new OpenLayers.Control.Zoom()
+    ];
+	var defaultLayerOptions = {
+		sphericalMercator : true,
+		layers : "0",
+		isBaseLayer : true,
+		projection : options.projection,
+		units : "m",
+		buffer : 3,
+		wrapDateLine : false
+	};
+//	options.restrictedExtent = new OpenLayers.Bounds(-179.0, 10.0, -42.0, 75.0).transform(WGS84_GEOGRAPHIC, WGS84_GOOGLE_MERCATOR);
+//	options.maxExtent = new OpenLayers.Bounds(-165.0, 10.0, -65.0, 65.0).transform(WGS84_GEOGRAPHIC, WGS84_GOOGLE_MERCATOR);
+	options.restrictedExtent = new OpenLayers.Bounds(-146.0698, 19.1647, -42.9301, 52.8949).transform(WGS84_GEOGRAPHIC, WGS84_GOOGLE_MERCATOR);
+		
+	var zyx = '/MapServer/tile/${z}/${y}/${x}';
+	var ArcGisLayer = function(name, identifier){
+		return new OpenLayers.Layer.XYZ(
+	 			name,
+	 			"http://services.arcgisonline.com/ArcGIS/rest/services/" + identifier + zyx, 
+	 			defaultLayerOptions
+		)
+	};
+	var mapLayers = [
+ 		ArcGisLayer('World Street Map', 'World_Street_Map'),
+ 		ArcGisLayer("World Topo Map",'World_Topo_Map'),
+ 		ArcGisLayer("World Image", "World_Imagery"),
+ 		ArcGisLayer("World Terrain Base", "World_Shaded_Relief")
+	];
+	var sitesLayerOptions = {};
+	Object.merge(
+		sitesLayerOptions, 
+		defaultLayerOptions
+	);
+	Object.merge(
+		sitesLayerOptions,
+		{
+			isBaseLayer: false
+		}
+	);
+	var extraUrlParams = {
+		layers : 'surface',
+		transparent: true,
+		tiled: true
+	};
+	
+	var sitesLayer = new OpenLayers.Layer.WMS(
+		'<a href="http://nationalatlas.gov/mld/sw9194x.html">NAWQA Surface Water Sites</a>',
+		'http://webservices.nationalatlas.gov/wms/water',
+		extraUrlParams,
+		sitesLayerOptions
+	);
+	
+	mapLayers.push(sitesLayer);
+	
+	options.layers = mapLayers;
+	var div = 'siteMap';
+	
+	map = new OpenLayers.Map(div, options);
+	map.zoomToExtent(options.restrictedExtent, true);
+}());
