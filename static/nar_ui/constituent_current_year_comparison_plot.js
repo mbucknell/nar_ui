289683b28f7448b5ae1b9ca6eed6b752b46d3c81
Tomcat Application Server <tomcat@cida-eros-nardev.er.usgs.gov>
2014-04-10 08:44:40 -0500

    automatic inclusion of the env and static files

---
+++ static/nar_ui/constituent_current_year_comparison_plot.js
@@ -0,0 +1,103 @@
+/**
+ * This file depends on:
+ * jquery.flot.js
+ * jquery.flot.resize.js
+ * jquery.flot.categories.js
+ * 
+ * plugin: jquery.flot.axislabels.js
+ * plugin: jquery.flot.tooltip.js
+ * 
+ */
+
+/**
+ * Do not invoke as a constructor (with the 'new' keyword).
+ * 
+ * @param {String} selector a jquery selector for the plot element
+ * @param {Object} series an object of the following format:
+ * {
+		constituentName: 'myConstituentName',
+		constituentUnit: '',
+		yearValue: 0,
+		yearColor: '#00FF00'
+		averageValue: 10,
+		averageColor: '#00FF00'
+	}
+   @param {String} legendSelector a jquery selector for the legend element
+ */
+
+var ConstituentCurrentYearComparisonPlot = function(plotContainerSelector, series){
+	
+	if(!$(plotContainerSelector).length){;
+		throw Error('Bar Chart could not find element with jquery selector "' + plotContainerSelector + '".');
+	}
+	
+	var plotContainer = $(plotContainerSelector);
+
+	var plotClass= ConstituentCurrentYearComparisonPlot.plotClass;
+	var plotDiv = $('<div/>', {'class': plotClass});;
+	var plotDivSelector = plotContainerSelector + ' .' + plotClass;
+	plotContainer.append(plotDiv);
+	
+	var legendClass = ConstituentCurrentYearComparisonPlot.legendClass;
+	var legendDiv = $('<div/>', {'class': legendClass });
+	var legendDivSelector = plotContainerSelector + ' .' + legendClass;
+	plotContainer.append(legendDiv);
+
+	
+	var yearSeries = {
+        data: [['', series.yearValue]],
+        label: '2014',
+        bars: {
+                show: true,
+                barWidth: 0.4,
+                align: "right",
+            	fillColor: series.yearColor
+            }
+    };
+	
+	var averageSeries = {
+        data : [['', series.averageValue]],
+		label : 'Average 1990-2013',
+		bars : {
+			show : true,
+			barWidth : 0.4,
+			align : "left",
+			fillColor: series.averageColor
+		}
+    };
+	
+	var flotSeries = [yearSeries, averageSeries];
+
+    var plot = $.plot(plotDivSelector, flotSeries, {
+        series: {
+            bars: {
+                show: true,
+                barWidth: 0.6,
+                align: "center"
+            }
+        },
+        xaxis: {
+            mode: "categories",
+            tickLength: 0,
+            axisLabel: series.constituentName
+        },
+        yaxis: {
+        	axisLabel: series.constituentUnit,
+        	axisLabelUseCanvas: true,
+            axisLabelPadding: 3
+        },
+        grid: { hoverable: true, clickable: true },
+        colors: [series.yearColor, series.averageColor],
+        tooltip: true,
+        tooltipOpts: {
+        	content: '%s: %y ' + series.constituentUnit 
+        },
+        legend: {
+        	container: legendDivSelector || null
+        }
+    });
+    return plot;
+};
+
+ConstituentCurrentYearComparisonPlot.legendClass = 'currentYearComparisonLegend';
+ConstituentCurrentYearComparisonPlot.plotClass = 'currentYearComparisonPlot';
