b02a8002343d0ae8d6afd0e044384459ddc6abca
Carl Schroedl <cschroedl@usgs.gov>
2014-04-10 16:57:28 -0500

    Both the site id and site name should now be in the summary report template context.

--- nar_ui/views.py
+++ nar_ui/views.py
@@ -1,6 +1,9 @@
 from django.views.generic.base import TemplateView
-from owslib.wfs import WebFeatureService
 from django.conf import settings
+import re
+from StringIO import StringIO
+from lxml import etree
+from pip._vendor import requests
 
 class HomePageView(TemplateView):
     template_name = 'nar_ui/home.html'
@@ -12,12 +15,35 @@ class SiteSummaryReportView(TemplateView):
     def get_context_data(self, **kwargs):
         
         context = super(SiteSummaryReportView, self).get_context_data(**kwargs)
-        siteId = context.get('siteId', '01646580')
+        site_id = context.get('siteId', '01646580')
         url = 'http://' + settings.GEOSERVER_HOST_NAME + settings.GEOSERVER_PATH + 'wfs'
-        wfs = WebFeatureService(url, '2.0.0')
-        typeName = 'NAR:NAWQA100_cy3fsmn'
         
-        feature = wfs.getfeature(typename=typeName, featureid=siteId, maxfeatures=1)
+        filter = """
+        <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+           <ogc:PropertyIsEqualTo>
+            <ogc:PropertyName>NAR:staid</ogc:PropertyName>
+            """
+        filter += '<ogc:Literal>' +site_id +'</ogc:Literal>' + """
+           </ogc:PropertyIsEqualTo>
+        </ogc:Filter>
+        """
+        filter = re.sub(r'\s{2,}', '',  filter)
+        
+        params = {
+                  'service': 'WFS',
+                  'version': '2.0.0',
+                  'request': 'GetFeature',
+                  'typeName': 'NAR:NAWQA100_cy3fsmn',
+                  'filter': filter,
+        }
+        
+        my_request = requests.get(url, params=params)
+        xml_pseudo_file = StringIO(my_request.content)
+        tree = etree.parse(xml_pseudo_file)
+        
+        site_name = tree.xpath('//NAR:staname', namespaces={'NAR':'http://cida.usgs.gov/NAR'})[0].text
+        
+        context['site_name'] = site_name
         
         return context
     
