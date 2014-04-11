1e04ac0628a66751ff7e251e7b2aa25c883a4e6c
Carl Schroedl <cschroedl@usgs.gov>
2014-04-11 12:41:10 -0500

    Moving data logic into model function.

--- nar_ui/models.py
+++ nar_ui/models.py
@@ -1,3 +1,34 @@
-from django.db import models
+import re
+from StringIO import StringIO
+from lxml import etree
+import requests
 
 # Create your models here.
+def get_site_name(site_id, url):
+    filter = """
+    <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+       <ogc:PropertyIsEqualTo>
+        <ogc:PropertyName>NAR:staid</ogc:PropertyName>
+        """
+    filter += '<ogc:Literal>' +site_id +'</ogc:Literal>' + """
+       </ogc:PropertyIsEqualTo>
+    </ogc:Filter>
+    """
+    #kill all whitespace except for one-length whitespace like the spaces between xml tag names and attribute names
+    filter = re.sub(r'\s{2,}', '',  filter)
+    
+    params = {
+              'service': 'WFS',
+              'version': '2.0.0',
+              'request': 'GetFeature',
+              'typeName': 'NAR:NAWQA100_cy3fsmn',
+              'filter': filter,
+    }
+    
+    my_request = requests.get(url, params=params)
+    xml_pseudo_file = StringIO(my_request.content)
+    tree = etree.parse(xml_pseudo_file)
+    
+    site_name = tree.xpath('//NAR:staname', namespaces={'NAR':'http://cida.usgs.gov/NAR'})[0].text
+
+    return site_name
--- nar_ui/views.py
+++ nar_ui/views.py
@@ -1,9 +1,6 @@
 from django.views.generic.base import TemplateView
 from django.conf import settings
-import re
-from StringIO import StringIO
-from lxml import etree
-from pip._vendor import requests
+import models
 
 class HomePageView(TemplateView):
     template_name = 'nar_ui/home.html'
@@ -17,32 +14,7 @@ class SiteSummaryReportView(TemplateView):
         context = super(SiteSummaryReportView, self).get_context_data(**kwargs)
         site_id = context.get('site_id', '01646580')
         url = 'http://' + settings.GEOSERVER_HOST_NAME + settings.GEOSERVER_PATH + 'wfs'
-        
-        filter = """
-        <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
-           <ogc:PropertyIsEqualTo>
-            <ogc:PropertyName>NAR:staid</ogc:PropertyName>
-            """
-        filter += '<ogc:Literal>' +site_id +'</ogc:Literal>' + """
-           </ogc:PropertyIsEqualTo>
-        </ogc:Filter>
-        """
-        filter = re.sub(r'\s{2,}', '',  filter)
-        
-        params = {
-                  'service': 'WFS',
-                  'version': '2.0.0',
-                  'request': 'GetFeature',
-                  'typeName': 'NAR:NAWQA100_cy3fsmn',
-                  'filter': filter,
-        }
-        
-        my_request = requests.get(url, params=params)
-        xml_pseudo_file = StringIO(my_request.content)
-        tree = etree.parse(xml_pseudo_file)
-        
-        site_name = tree.xpath('//NAR:staname', namespaces={'NAR':'http://cida.usgs.gov/NAR'})[0].text
-        
+        site_name = models.get_site_name(site_id, url)
         context['site_name'] = site_name
         
         return context
