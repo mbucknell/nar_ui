db019e10af675aec62c3f272a663621bc4ddf6ef
Carl Schroedl <cschroedl@usgs.gov>
2014-05-14 14:51:19 -0500

    Adding new mock data sets, renaming existing ones.

--- nar_common/static/nar_ui/full_report/generate_mock_data.js
+++ nar_common/static/nar_ui/full_report/generate_mock_data.js
@@ -1,11 +1,13 @@
 // copy and paste this into a browser console to run.
 // requires sugarjs
-
-    var mockData = function(minYear, maxYear, minValue, maxValue){
+    /**
+     * @param {string} frequency - a singularized string representing a unit of time ex :'year', 'month', 'day' 
+     */
+    var mockData = function(minYear, maxYear, minValue, maxValue, frequency){
         var mockDataSet = [];
         var minYearDate = Date.create(''+minYear);
         var maxYearDate = Date.create(''+maxYear); 
-        Date.range(minYearDate, maxYearDate).every("month", function(date){
+        Date.range(minYearDate, maxYearDate).every(frequency, function(date){
             var value = Number.random(minValue, maxValue);
             var timestamp = date.getTime();
             mockDataSet.push([timestamp, value]);
@@ -14,8 +16,10 @@
     };
     
     var mockDataParameters = [
-                              [1983, 2000, 0, 100],
-                              [1992, 2012, 0, 1000]
+                              [1983, 2000, 0, 100, 'month'],
+                              [1992, 2012, 0, 1000, 'month'],
+                              [1993, 2010, 0, 50, 'year'],
+                              [1985, 2005, 0, 25, 'year']
                               ];
     var mockDataSets = mockDataParameters.map(function(parameters){
         return {
