cf42078b9a731264fe17902d8b58b569836ad5a2
Carl Schroedl <cschroedl@usgs.gov>
2014-06-19 14:10:40 -0500

    Re-organizing.

---
+++ nar_common/static/nar_ui/summary_report/main.js
@@ -0,0 +1,52 @@
+$(document).ready(
+        function() {
+
+            var nitrateSeries = {
+                    constituentName : nar.Constituents.nitrate.name,
+                    constituentUnit : 'Million Tons',
+                    yearValue : 12,
+                    yearColor : nar.Constituents.nitrate.color,
+                    averageName : 'Average 1991-2013',
+                    averageValue : 10
+            };
+
+            var nitrateGraph = ConstituentCurrentYearComparisonPlot(
+                    '#barChart2', nitrateSeries);
+
+            var phosphorousSeries = {
+                    constituentName : nar.Constituents.total_phosphorous.name,
+                    constituentUnit : 'Million Tons',
+                    yearValue : 100,
+                    yearColor : nar.Constituents.total_phosphorous.color,
+                    averageName : 'Average 1985-2013',
+                    averageValue : 153
+            };
+
+            var phosphorousGraph = ConstituentCurrentYearComparisonPlot(
+                    '#barChart3', phosphorousSeries);
+
+            var streamflowSeries = {
+                    constituentName : nar.Constituents.streamflow.name,
+                    constituentUnit : 'Million acre-feet',
+                    yearValue : 1.7,
+                    yearColor : nar.Constituents.streamflow.color,
+                    averageName : 'Average 1999-2013',
+                    averageValue : 2.4
+            };
+
+            var streamflowGraph = ConstituentCurrentYearComparisonPlot(
+                    '#barChart1', streamflowSeries);
+
+            var sedimentSeries = {
+                    constituentName : nar.Constituents.suspended_sediment.name,
+                    constituentUnit : 'Million Tons',
+                    yearValue : 300,
+                    yearColor : nar.Constituents.suspended_sediment.color,
+                    averageName : 'Average 1990-2013',
+                    averageValue : 100
+            };
+
+            var sedimentGraph = ConstituentCurrentYearComparisonPlot(
+                    '#barChart4', sedimentSeries);
+
+        });
