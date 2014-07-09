29ff04997a8350c4d5f7d28063919b731fcda540
Carl Schroedl <cschroedl@usgs.gov>
2014-07-09 15:12:15 -0500

    Concentration plot -- removing grid in favor of tick marks. Using log scale.

--- nar_common/static/nar_ui/full_report/sample_concentration_plot.js
+++ nar_common/static/nar_ui/full_report/sample_concentration_plot.js
@@ -42,6 +42,7 @@ nar.fullReport = nar.fullReport || {};
             xaxis: {
                 mode: 'time',
                 timeformat: "%Y/%m",
+                tickLength: 10,
                 minTickSize: [1, 'month']
             },
             yaxis: {
@@ -50,8 +51,10 @@ nar.fullReport = nar.fullReport || {};
                 axisLabelFontSizePixels: 12,
                 axisLabelFontFamily: "Verdana, Arial, Helvetica, Tahoma, sans-serif",
                 axisLabelPadding: 5,
-                ticks: [1, 5, 10, 50, 100, 500, 1000],
+                ticks: [0.001,0.01,0.1,1,10,100, 1000],
                 tickDecimals: 2,
+                tickLength: 10,
+                min: 0.001,
                 transform: function(value){
                     if(0 >= value){
                         return 0;
@@ -62,10 +65,11 @@ nar.fullReport = nar.fullReport || {};
                 },
                 inverseTransform: function(value){
                     return Math.pow(logBase, value);
-                }
+                } 
             },
             grid:{
-                hoverable: true
+                hoverable: true,
+                autoHighlight: true
             },
             legend: {
                    show: false
