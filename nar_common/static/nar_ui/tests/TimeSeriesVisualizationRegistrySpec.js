fa5963e26f85036005fa9f5aebfeb4a1506b57af
Carl Schroedl <cschroedl@usgs.gov>
2014-07-30 17:59:33 -0500

    First step of removing order dependence from TimeSeriesVisualizationRegistry Tests.

--- nar_common/static/nar_ui/tests/TimeSeriesVisualizationRegistrySpec.js
+++ nar_common/static/nar_ui/tests/TimeSeriesVisualizationRegistrySpec.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
 	describe('nar.fullReport.TimeSeriesVisualizationRegistry', function(){
-		var tsvRegistry = nar.fullReport.TimeSeriesVisualizationRegistry;
-		var tsvId = 'purple';
-		//html fixtures:
+		var tsvRegistry,
+		tsv,
+		tsvId,
+		elements;
 		var instructionsElement = $('<div class="instructions"></div>');
 		var allPlotsWrapperElement = $('<div class="plotsWrapper"></div>');
-		$('body').append([instructionsElement, allPlotsWrapperElement]);
-		
-		var tsv = new nar.fullReport.TimeSeriesVisualization({
+		elements = [instructionsElement, allPlotsWrapperElement];
+		$('body').append(elements);
+		tsvRegistry = nar.fullReport.TimeSeriesVisualizationRegistry;
+		tsvId = 'purple';
+		tsv = new nar.fullReport.TimeSeriesVisualization({
 			id: tsvId,
 			plotter: function(){
 				throw Exception('not yet implemented');
@@ -16,34 +19,26 @@ $(document).ready(function(){
 			instructionsElt: instructionsElement,
 			allPlotsWrapperElement: allPlotsWrapperElement
 		});
-		
-		describe('register', function(){
-			it('should register previously unregistered TSVs without error', function(){
+
+			it('should register() previously unregistered TSVs without error', function(){
 				expect(function(){tsvRegistry.register(tsv);}).not.toThrow();
 			});
-			it('should throw exceptions if a user attempts to register a previously registered TSV', function(){
+			it('should throw exceptions if a user attempts to register() a previously registered TSV', function(){
 				expect(function(){tsvRegistry.register(tsv);}).toThrow();
 			});
-		});
-		describe('get', function(){
-			it('returns undefined if an unknown id is requested', function(){
+			it('gets() undefined if an unknown id is requested', function(){
 				expect(tsvRegistry.get('totallyBogusId!')).toBeUndefined();
 			});
-			it('returns a TSV if a known id is requested', function(){
+			it('gets() a TSV if a known id is requested', function(){
 				expect(tsvRegistry.get(tsvId)).toBe(tsv);
 			});
-		});
-		describe('getAll', function(){
-			it('returns the correct number of TSVs', function(){
+			it('returns the correct number of TSVs when calling getAll()', function(){
 				expect(tsvRegistry.getAll().length).toBe(1);
 			});
-		});
-		describe('stripUrlPrefix', function(){
 			it('strips url prefixes', function(){
 				var urlSuffix = 'a';
 				var mockUrl = self.urlPrefix + urlSuffix;
 				expect(tsvRegistry.stripUrlPrefix(mockUrl)).toBe(urlSuffix);
 			});
-		});
 	});
 });
