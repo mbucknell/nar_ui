1c4f00039b68c1f120488e27db7b82a69d539864
Jordan Walker <jiwalker@usgs.gov>
2014-07-30 19:10:53 -0500

    NAWQAAN-207
    * Some additional mapCommons for WFS
    * Removed hard-coded sites
    * siteFilter does WFS request and builds site rows
    * need to filter yet
    * also, I think it is limiting to 100 features

--- nar_common/static/nar_ui/siteFilter.js
+++ nar_common/static/nar_ui/siteFilter.js
@@ -5,7 +5,16 @@ var nar = nar || {};
 		selectedTypes = $.makeArray($("input[name='" + siteFilterName + "']:checked").map(function(){return $(this).val()}));
 		return selectedTypes;
 	};
+	var makeTd = function(content) {
+		var $td = $('<td />').html(content);
+		return $td;
+	};
 	nar.siteFilter = {
+		Site : function(id, name, type) {
+			this.id = id;
+			this.name = name;
+			this.type = type;
+		},
 		writeCQLFilter : function() {
 			var selectedTypes = getSelectedTypes();
             var cqlFilter = "c3type IS NOT NULL";
@@ -18,6 +27,35 @@ var nar = nar || {};
 		},
 		addChangeHandler : function(fn) {
 			$("input[name='" + siteFilterName + "']").change(fn);
+		},
+		buildRow : function(site) {
+			var $tableRow = $('<tr />').addClass('clickableRow')
+				.attr('href', CONFIG.baseUrl + 'site/' + site.id + '/summary-report');
+			$tableRow.append(makeTd(site.id))
+				.append(makeTd(site.name))
+				.append(makeTd(site.type));
+			$tableRow.click(function() {
+	            window.document.location = $(this).attr("href");
+			});
+			return $tableRow;
+		},
+		onEachSite : function(doThis) {
+			var protocol = nar.commons.mapUtils.createSitesFeatureProtocol();
+			var filter = null;//new OpenLayers.Filter({}); // will add cql filter later
+			nar.commons.mapUtils.getData(protocol, filter, function(response) {
+				if (response.success()) {
+					response.features.each(function(feature) {
+						var site = new nar.siteFilter.Site(feature.data.staid, feature.data.staname, feature.data.c3type);
+						doThis(site);
+					});
+				}
+			});
+		},
+		loadSitesToDom : function() {
+			var $table = $('.site_table');
+			nar.siteFilter.onEachSite(function(site) {
+				$table.append(nar.siteFilter.buildRow(site));
+			});
 		}
 	};
 }());
