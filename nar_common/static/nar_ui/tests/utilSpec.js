08f04c6e5d55fb161057f0ea017d2aa19650c2a9
Carl Schroedl <cschroedl@usgs.gov>
2014-08-01 14:20:57 -0500

    Revert "Dummy test failure to test jenkins pull request builder."
    This reverts commit 045c5bb99147bce26aa1ea7d17de7148e2dfe540.

--- nar_common/static/nar_ui/tests/utilSpec.js
+++ nar_common/static/nar_ui/tests/utilSpec.js
@@ -1,7 +1,6 @@
 describe('Tests for nar.util', function() {
 	
 	it('Expects utility functions to be defined', function() {
-		expect(true).toBe(false);
 		expect(nar.util.assert_selector_present).toBeDefined();
 		expect(nar.util.getTimeStamp).toBeDefined();
 	});
