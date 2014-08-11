8fdb9f3b93e384e4cd1a0ab4097d3d1b92df4e61
Mary Bucknell <mbucknell@usgs.gov>
2014-08-11 15:52:09 -0500

    Added a new app, utils, which we can use for reusable code which does not belong to a single app.
    Added a templatetags directory to the utils app and created a
    'pull_feed' inclusion tag. This allows us to pull a feed into any
    template where we want it.

---
+++ utils/templatetags/feeds.py
@@ -0,0 +1,34 @@
+
+import feedparser
+
+from bs4 import BeautifulSoup
+
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag('utils/feed_template.html')
+def pull_feed (feed_url):
+    feed = feedparser.parse(feed_url)
+    
+    #Procss html to remove unwanted mark-up and fix links
+    if len(feed) > 0:
+        soup = BeautifulSoup(feed['entries'][0].summary)
+        
+        # Remove edited by paragraph
+        soup.p.extract()
+        
+        # Remove final div in the feed
+        feedDiv = soup.find('div', class_='feed')
+        childrenDivs = feedDiv.findAll('div')
+        childrenDivs[len(childrenDivs) - 1].extract()
+        
+        # Translate any in page links to use relative URL
+        base = feed['entries'][0].summary_detail.base        
+        links = feedDiv.select('a[href^="' + base + '"]')
+        for link in links:
+            link['href'] = link['href'].replace(base, '') 
+            
+        post = unicode(soup)
+    
+    return {'post' : post};
