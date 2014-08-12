3383e7ef6fd2ad5baaf0cdc8c669d9417578f0a5
Mary Bucknell <mbucknell@usgs.gov>
2014-08-12 13:04:59 -0500

    Added tests for the templatetags pull_feed method and corrected a couple of issues with the method.

--- utils/templatetags/feeds.py
+++ utils/templatetags/feeds.py
@@ -10,25 +10,26 @@ register = template.Library()
 @register.inclusion_tag('utils/feed_template.html')
 def pull_feed (feed_url):
     feed = feedparser.parse(feed_url)
-    
-    #Procss html to remove unwanted mark-up and fix links
-    if len(feed) > 0:
+
+    # Process html to remove unwanted mark-up and fix links
+    post = ''
+    if len(feed['entries']) > 0:
         soup = BeautifulSoup(feed['entries'][0].summary)
-        
+
         # Remove edited by paragraph
         soup.p.extract()
-        
+
         # Remove final div in the feed
         feedDiv = soup.find('div', class_='feed')
         childrenDivs = feedDiv.findAll('div')
         childrenDivs[len(childrenDivs) - 1].extract()
-        
+
         # Translate any in page links to use relative URL
-        base = feed['entries'][0].summary_detail.base        
+        base = feed['entries'][0].summary_detail.base
         links = feedDiv.select('a[href^="' + base + '"]')
         for link in links:
-            link['href'] = link['href'].replace(base, '') 
-            
+            link['href'] = link['href'].replace(base, '')
+
         post = unicode(soup)
-    
-    return {'post' : post};
+
+    return {'post' : post}
