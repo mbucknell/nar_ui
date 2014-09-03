fa579df54ac297adf31c9e797894a1bc1c3f07fc
Carl Schroedl <cschroedl@usgs.gov>
2014-09-03 16:50:40 -0500

    Adding MRB contributions lookup web service

--- helpcontent/views.py
+++ helpcontent/views.py
@@ -3,11 +3,11 @@ import simplejson
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.forms import model_to_dict
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseBadRequest
 from django.views.generic import View, ListView, TemplateView
 from django.views.generic.base import ContextMixin 
 
-from .models import Definition, SiteLinkInfo, SiteEcoregionCriteriaInfo
+from .models import Definition, SiteLinkInfo, SiteEcoregionCriteriaInfo, MrbSubBasinContributions
 
 
 class FeedContextMixin(ContextMixin):
@@ -131,3 +131,36 @@ class SiteInfoJsonView(View):
             return HttpResponse(simplejson.dumps(result), content_type='application/json')
         
         raise Http404
+    
+    
+    
+class MrbSubBasinContributionsJsonView(View):
+    query_params = ['constituent', 'parameter_type', 'water_year']
+    
+    def get(self, request, *args, **kwargs):
+        """given adequate query params, return a map of basin name to contribution amount"""
+        
+        safe_query_values = {}
+        
+        for query_param in MrbSubBasinContributionsJsonView.query_params:
+            if query_param in request.GET:
+                safe_query_values[query_param] = request.GET[query_param]
+            else:
+                #require all query parameters
+                return HttpResponseBadRequest('Missing Parameter - "{}"'.format(query_param))
+            
+        if 0 == len(safe_query_values):
+            raise Http404
+        
+        try:
+            mrbSubBasinContributions = MrbSubBasinContributions.objects.filter(**safe_query_values)
+            if(0 == len(mrbSubBasinContributions)):
+                raise Http404
+            results = {}
+            for mrbSubBasinContribution in mrbSubBasinContributions:
+                results[mrbSubBasinContribution.basin_name] = mrbSubBasinContribution.contribution
+
+            return HttpResponse(simplejson.dumps(results), content_type='application/json')
+        except ObjectDoesNotExist:
+                raise Http404
+
