2d3e65b110daddd304bc4dd9c4a26dae14ca9e34
Carl Schroedl <cschroedl@usgs.gov>
2014-09-04 13:42:19 -0500

    Approaching full test coverage on new mrb contribution service.

--- helpcontent/test/test_views.py
+++ helpcontent/test/test_views.py
@@ -1,9 +1,9 @@
 from django.test import SimpleTestCase, Client, TestCase
 from django.core.urlresolvers import reverse
-from django.http import Http404, HttpResponseBadRequest
+from django.http import HttpResponseNotFound, HttpResponseBadRequest
 from ..models import MrbSubBasinContributions
-
-from ..views import FeedContextMixin
+import json
+from ..views import FeedContextMixin, MrbSubBasinContributionsJsonView
 
 class FeedContextMixinTestCase(SimpleTestCase):
     
@@ -38,12 +38,38 @@ class MrbContributionsJsonViewTestCase(TestCase):
         response = client.get(reverse('helpcontent-mrbSubBasinContributions'), params)
         
         return response
+
     def setUp(self):
         
         MrbSubBasinContributions.objects.create(**self.contribution)
         
-    def test_missing_parameters(self):
+    def test_empty_parameters(self):
         
         response = self.getContributions({})
         self.assertIsInstance(response, HttpResponseBadRequest)
+        
+    def test_missing_parameters(self):
+        #get all but one of the required param names
+        parameter_keys = MrbSubBasinContributionsJsonView.query_params[:-1]
+        params = {}
+        for key in parameter_keys:
+            params[key] = 'dummy'
+            
+        response = self.getContributions(params)
+        self.assertIsInstance(response, HttpResponseBadRequest)
+        
+    def test_valid_parameters_but_no_matching_record(self):
+        params = dict(self.contribution)
+        #override one of the values so that the query fails
+        params[params.keys()[0]] = 'dummy'
+
+        self.assertIsInstance(self.getContributions(params), HttpResponseNotFound)
+    
+    def test_valid_parameters_and_matching_record(self):
+        response = self.getContributions(self.contribution)
+        self.assertEqual(response.status_code, 200)
+        response_map = json.loads(response.content)
+        expected_contribution_map = {}
+        expected_contribution_map[self.contribution['basin_name']] = self.contribution['contribution'] 
+        self.assertDictEqual(response_map, expected_contribution_map)
         
