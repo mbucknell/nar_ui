3bc39ce9f142540328aae6a1cbf7af4306bcb554
Suftin, Ivan <isuftin@usgs.gov>
2014-09-18 12:21:49 -0500

    Removing criteria line on sample plots
    Removed criteria line on sample plots for all except NO23 plots

--- nar_common/static/nar_ui/full_report/sample_concentration_plot.js
+++ nar_common/static/nar_ui/full_report/sample_concentration_plot.js
@@ -16,6 +16,10 @@ nar.fullReport = nar.fullReport || {};
         var previousYearsColor = miscConstituentInfo.colors.previousYears;
         var currentYearColor= miscConstituentInfo.colors.currentYear;
         var criteriaLineColor = miscConstituentInfo.colors.criteriaLine;
+        var constituentId = tsViz.getComponentsOfId().constituent;
+        // We only show a criteria line on sample reports if the constituent is Nitrate
+        var useCriteriaLine = constituentId === 'nitrate';
+        var constituentToCriteria;
         
         var makeSeriesConfig = function(dataSet, color){
             return {
@@ -31,50 +35,35 @@ nar.fullReport = nar.fullReport || {};
             };   
         };
         
-
-		var constituentToCriteria = {
-			nitrogen : nar.siteHelpInfo.tn_criteria,
-			phosphorus : nar.siteHelpInfo.tp_criteria,
-			nitrate : 10
-		};
-        
-
-		var criteriaLineDescription = function(constituentId) {
-			var result;
-			if (constituentId === 'nitrogen' || constituentId === 'phosphorus') {
-				result = 'EPA Ecoregion ' + nar.siteHelpInfo.nutrient_ecoregion + 
-					' Recommended Nutrient Criteria, Total ' + constituentId + ' = ' + 
-					constituentToCriteria[constituentId] + ' mg/L.';
-			} else {
-				result = 'EPA Maximum Contaminant Level (MCL) = ' + constituentToCriteria[constituentId] + ' mg/L as N.';
-			}
-
-			return result + ' See Constituents Measured above for more information.';
-        };
-        
-		var constituentId = tsViz.getComponentsOfId().constituent;
-		var criteriaLineValue = constituentToCriteria[constituentId] || null;
-        
-        var criteriaLineSeries = {
-            label: constituentName,
-            data: [[nar.fullReport.PlotUtils.YEAR_NINETEEN_HUNDRED,criteriaLineValue],
-                   [nar.fullReport.PlotUtils.ONE_YEAR_IN_THE_FUTURE,criteriaLineValue]],
-            lines: {
-                show: true,
-                fillColor: criteriaLineColor,
-                lineWidth: 1
-            },
-            shadowSize: 0
-        };
-        
         var previousYearsSeries = makeSeriesConfig(previousYearsData, previousYearsColor);
         var currentYearSeries = makeSeriesConfig(currentYearData, currentYearColor);
-        
         var series = [
           previousYearsSeries,
-          currentYearSeries,
-          criteriaLineSeries
+          currentYearSeries
         ];
+        
+        if (useCriteriaLine) {
+        	constituentToCriteria = {
+    			nitrogen : nar.siteHelpInfo.tn_criteria,
+    			phosphorus : nar.siteHelpInfo.tp_criteria,
+    			nitrate : 10
+    		};
+        	var criteriaLineValue = constituentToCriteria[constituentId] || null;
+        	var criteriaLineSeries = {
+                    label: constituentName,
+                    data: [[nar.fullReport.PlotUtils.YEAR_NINETEEN_HUNDRED,criteriaLineValue],
+                           [nar.fullReport.PlotUtils.ONE_YEAR_IN_THE_FUTURE,criteriaLineValue]],
+                    lines: {
+                        show: true,
+                        fillColor: criteriaLineColor,
+                        lineWidth: 1
+                    },
+                    shadowSize: 0
+                };
+        	
+        	series.add(criteriaLineSeries);
+        }
+        
         var logBase = 10;
         var logFactor = Math.log(logBase);
         var plot = $.plot(plotContainer, series, {
@@ -114,9 +103,16 @@ nar.fullReport = nar.fullReport || {};
             },
             colors:[previousYearsColor, currentYearColor, criteriaLineColor]
         });
-        var hoverFormatter = nar.fullReport.PlotUtils.utcDatePlotHoverFormatter;
-        nar.fullReport.PlotUtils.setPlotHoverFormatter(plotContainer, hoverFormatter);
-        nar.fullReport.PlotUtils.setLineHoverFormatter(plotContainer, criteriaLineValue, criteriaLineDescription(constituentId));
+        
+        if (useCriteriaLine) {
+    		var criteriaLineDescription = 'EPA Maximum Contaminant Level (MCL) = ' 
+    			+ constituentToCriteria[constituentId] 
+    			+ ' mg/L as N.<br />See Constituents Measured above for more information.';
+            
+	        nar.fullReport.PlotUtils.setPlotHoverFormatter(plotContainer, nar.fullReport.PlotUtils.utcDatePlotHoverFormatter);
+	        nar.fullReport.PlotUtils.setLineHoverFormatter(plotContainer, criteriaLineValue, criteriaLineDescription);
+        }
+        
         return plot;
     };    
 }());
