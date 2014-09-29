bb0a756bedeb8afaee9fb05e76f88c511bddf8d6
Carl Schroedl <cschroedl@usgs.gov>
2014-09-29 13:46:07 -0500

    Improved clarity and robustness of unobserve method. Removed 'handler' variables and documentation in favor of observer/subject vocab.

--- nar_common/static/nar_ui/commons/Subject.js
+++ nar_common/static/nar_ui/commons/Subject.js
@@ -10,41 +10,47 @@ var nar = nar || {};
 nar.commons = nar.commons || {};
 nar.commons.Subject = function(){
 	var self = this;
-	var handlers = [];
+	var observers = [];
 	
 	/**
-	 * @param {function} handler
+	 * @param {function} observer
 	 */
-	self.observe = function (handler) {
+	self.observe = function (observer) {
 		console.log('added new observer');
-		handlers.push(handler);
+		observers.push(observer);
 	};
 	/**
-	 * @param {function} handler
-	 * @returns {Boolean} true if handler was observing, false if it was not observing
+	 * Prevent the specified observer from receiving future notifications
+	 * If the observer was registered multiple times, remove all occurrences of it
+	 * @param {function} observer
+	 * @returns {Boolean} true if the observer was observing, false if it was not observing
 	 */
-	self.unobserve = function (handler) {
-		var handlerIndex = handlers.indexOf(handler);
-		var handlerWasFound = false;
-		if(-1 !== handlerIndex){
-			handlers.removeAt(handlerIndex);
-			handlerWasFound = true;
-			console.log('removed observer');
-		}
-		return handlerWasFound;
+	self.unobserve = function (observerToRemove) {
+		var originalLength = observers.length;	
+		observers.remove(function(observer){
+			if(observer === observerToRemove){
+				console.log('removed observer');
+				return true;
+			}
+			else{
+				return false;
+			}
+		});
+
+		return originalLength !== observers.length;
 	};
 	/**
 	 * @param * - any arguments you specify will be 
-	 * passed to observing handlers
-	 * @return {Integer} the number of handlers called
+	 * passed to observers
+	 * @return {Integer} the number of observers notified
 	 */
-	self.notify = function notifyObservers() {
+	self.notify = function () {
 		var args = Array.create(arguments);
-		handlers.map(function(handler){
-			handler.apply(null, args);
+		observers.map(function(observer){
+			observer.apply(null, args);
 		});
-		console.log('notified ' + handlers.length + ' observers');
-		return handlers.length;
+		console.log('notified ' + observers.length + ' observers');
+		return observers.length;
 	};
 };
 
