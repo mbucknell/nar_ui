ecc97e1d82ffb7a3c9658e9aacff33fcd8cdd7f7
Mark McDermond <mamcderm@usgs.gov>
2014-12-02 15:37:28 -0700

    NAWQAAN-381; revised approach

--- nar_common/static/nar_ui/summary_report/main.js
+++ nar_common/static/nar_ui/summary_report/main.js
@@ -1,6 +1,14 @@
 //@requires nar.timeSeries.VisualizationRegistry, nar.timeSeries.Visualization
 $(document).ready(
 	function() {
+		var id = PARAMS.siteId;
+		CONFIG.startWaterYear = 1993;
+		var currentDataTimeRange = new nar.timeSeries.TimeRange(
+				nar.WaterYearUtils.getWaterYearStart(CONFIG.currentWaterYear, true),
+				nar.WaterYearUtils.getWaterYearEnd(CONFIG.currentWaterYear, true));
+		var averageDataTimeRange = new nar.timeSeries.TimeRange(
+				nar.WaterYearUtils.getWaterYearStart(CONFIG.startWaterYear, true),
+				nar.WaterYearUtils.getWaterYearEnd(CONFIG.currentWaterYear, true));
 		var ConstituentCurrentYearComparisonPlot = nar.plots.ConstituentCurrentYearComparisonPlot;
 		var ExceedancePlot = nar.plots.ExceedancePlot;            
 
@@ -45,12 +53,63 @@ $(document).ready(
 					};
 				}
 			};
-                       
-			//variables to hold water year values to plot 
-			var nitrateSeriesYearValue = 0;
-			var phosphorusSeriesYearValue = 0;
-			var streamflowSeriesYearValue = 0;
-			var sedimentSeriesYearValue = 0;
+			
+			/*
+			 * @param {String} - feature for which data should be retrieved
+			 * @param {Array} availability of data availability objects to be used to retrieve data
+			 * @return TimeSeries.Collection containing a time series for each object in availability
+			 */
+			var getTimeSeriesCollection = function(forFeature, availability) {
+				var result = new nar.timeSeries.Collection();
+				
+				var thisFeatureAvailability = availability.filter(function (value) {
+					return value.featureOfInterest === forFeature;
+				});
+				
+				thisFeatureAvailability.each(function(value) {
+					var ts = new nar.timeSeries.TimeSeries({
+						timeRange : averageDataTimeRange,
+						observedProperty : value.observedProperty,
+						procedure : value.procedure,
+						featureOfInterest : value.featureOfInterest
+					});
+					result.add(ts);
+				});
+				return result;
+			};
+            
+			var getPlotValues = function(tsCollections) {			
+				var avgData = [];
+				var currentYearData = [];
+				
+				var dataValue = function(dataPoint) {
+					return parseFloat(dataPoint[1]);
+				};
+				
+				// Create data series for each collection for the avg up to the current water year and the current year.
+				tsCollections.forEach(function(tsC) {
+					var sortedData = tsC.getDataMerged();
+					var splitData = nar.plots.PlotUtils.getDataSplitIntoCurrentAndPreviousYears(sortedData);
+					
+					if (splitData.previousYearsData.length === 0) {
+						avgData.push(0);
+					}
+					else {
+						avgData.push(splitData.previousYearsData.average(dataValue));
+					}
+					if (splitData.currentYearData.length === 0) {
+						currentYearData.push(0);
+					}
+					else {
+						currentYearData.push(dataValue(splitData.currentYearData.first()));
+					}	
+				});
+				
+				return {
+					average : avgData,
+					current : currentYearData
+				};
+			};
 
 			//find out what data is available for the site
 			var getDataAvailability = $.ajax({
@@ -62,11 +121,17 @@ $(document).ready(
 					'request' : 'GetDataAvailability',
 					'service' : 'SOS',
 					'version' : '2.0.0',
-					'featureOfInterest' : PARAMS.siteId
+					'featureOfInterest' : id
 				})
 			});
-        	
+
+			var streamflowDataAvailability = [];
+			var nitrateDataAvailability = [];
+			var phosphorusDataAvailability = [];
+			var sedimentDataAvailability = [];
+			
 			var successfulGetDataAvailability = function(data, textStatus, jqXHR) {
+
 				var dataAvailability = data.dataAvailability;
         				
 				dataAvailability.each(function(dataAvailability) {
@@ -76,131 +141,168 @@ $(document).ready(
 					if(procedure.endsWith('COMP')){
 						return;//continue
 					}
-					else
-					{	
-						//not sure if these are the correct procedures and properties needed
-						if((procedure.endsWith('discrete_concentration') &&
-								observedProperty.endsWith('NO23') ||
-								observedProperty.endsWith('TP') ||
-								observedProperty.endsWith('SSC')) ||
-								procedure.endsWith('annual_flow') && 
-								observedProperty.endsWith('Q')	) {
-                    		
-							var timeSeries = new nar.timeSeries.TimeSeries(
-								{
-								observedProperty : observedProperty,
-								procedure : procedure,
-								featureOfInterest: PARAMS.siteId,
-								timeRange : new nar.timeSeries.TimeRange(
-										nar.WaterYearUtils.getWaterYearStart(CONFIG.currentWaterYear, true),
-										nar.WaterYearUtils.getWaterYearEnd(CONFIG.currentWaterYear, true))
-								});
-        					
-							timeSeries.retrieveData().then(
-								function(response) {
-									switch (procedure+observedProperty) {
-									
-									case CONFIG.sosDefsBaseUrl + 'procedure/discrete_concentration' + 
-										CONFIG.sosDefsBaseUrl + 'property/NO23':
+					else if (procedure.endsWith('discrete_concentration') &&
+							observedProperty.endsWith('NO23')) {
 
-										//need to know how to handle remark data and also 
-										//how to calculate graph value
-										nitrateSeriesYearValue = response.data[0][1];	
-										var nitrateSeries = {
-											constituentName : nar.Constituents.nitrate.name,
-											constituentUnit : 'Million Tons',
-											yearValue : nitrateSeriesYearValue ? nitrateSeriesYearValue : 8,
-											yearColor : nar.Constituents.nitrate.color,
-											averageName : 'Average 1991-2013',
-											averageValue : 10
-										};
+						var timeSeries = new nar.timeSeries.TimeSeries(
+						{
+							observedProperty : observedProperty,
+							procedure : procedure,
+							featureOfInterest: id,
+							timeRange : currentDataTimeRange
+						});
+	        					
+						timeSeries.retrieveData().then(
+							function(response) {
+								//warning, this is broken, remark data not being handled! 											
+								var result = calculateExceedance(response.data);
+								var humanHealthExceedancePlot = ExceedancePlot('humanHealthExceedances', 
+								[
+									{constituent: nar.Constituents.nitrate, data: result.value, label: result.label},
+									{constituent: {color: '', name: ' '}, data: ' ', label: ['']}
+								],
+								exceedancesTitle
+								);
+		        			},
+	        				function(reject) {
+	        					throw Error ('Could not retrieve discrete data');
+	        				}
+	        			);
+					}
+					else if (procedure.endsWith('annual_flow') && 
+							observedProperty.endsWith('Q')) {
 
-										var nitrateGraph = ConstituentCurrentYearComparisonPlot(
-												'#barChart2', nitrateSeries);
-										
-										var result = calculateExceedance(response.data);
-										var humanHealthExceedancePlot = ExceedancePlot('humanHealthExceedances', 
-											[
-											 {constituent: nar.Constituents.nitrate, data: result.value, label: result.label},
-											 {constituent: {color: '', name: ' '}, data: ' ', label: ['']}
-											],
-											exceedancesTitle
-										);
-										break;
-										
-									case CONFIG.sosDefsBaseUrl + 'procedure/discrete_concentration' + 
-										CONFIG.sosDefsBaseUrl + 'property/TP':
-
-										//need to know how to calculate graph value
-										phosphorusSeriesYearValue = response.data[0][1];	
-										var phosphorusSeries = {
-											constituentName : nar.Constituents.phosphorus.name,
-											constituentUnit : 'Million Tons',
-											yearValue : phosphorusSeriesYearValue ? phosphorusSeriesYearValue : 8,
-											yearColor : nar.Constituents.phosphorus.color,
-											averageName : 'Average 1985-2013',
-											averageValue : 153
-										};
-
-										var phosphorusGraph = ConstituentCurrentYearComparisonPlot(
-												'#barChart3', phosphorusSeries);
-										break;
-										
-									case CONFIG.sosDefsBaseUrl + 'procedure/discrete_concentration' + 
-										CONFIG.sosDefsBaseUrl + 'property/SSC':
-
-										//need to know how to calculate graph value
-										sedimentSeriesYearValue = response.data[0][1];	
-										var sedimentSeries = {
-											constituentName : nar.Constituents.sediment.name,
-											constituentUnit : 'Million Tons',
-											yearValue : sedimentSeriesYearValue ? sedimentSeriesYearValue : 8,
-											yearColor : nar.Constituents.sediment.color,
-											averageName : 'Average 1990-2013',
-											averageValue : 100
-										};
-
-										var sedimentGraph = ConstituentCurrentYearComparisonPlot(
-												'#barChart4', sedimentSeries);
-										break;
-										
-									case CONFIG.sosDefsBaseUrl + 'procedure/annual_flow' + 
-										CONFIG.sosDefsBaseUrl + 'property/Q':
-										//find out how to get convert to million acre feet
-										streamflowSeriesYearValue = response.data[0][1]/10000;	
-										var streamflowSeries = {
-											constituentName : nar.Constituents.streamflow.name,
-											constituentUnit : 'Million acre-feet',
-											yearValue : streamflowSeriesYearValue ? streamflowSeriesYearValue : 8,
-											yearColor : nar.Constituents.streamflow.color,
-											averageName : 'Average 1999-2013',
-											averageValue : 2.4
-										};
-
-										var streamflowGraph = ConstituentCurrentYearComparisonPlot(
-												'#barChart1', streamflowSeries);
-										break;
-										
-									default:
-										break;
-        							}
-        						},
-        						function(reject) {
-        							throw Error ('Could not retrieve discrete data');
-        						}
-        					);
-                    	}
+						streamflowDataAvailability.push(dataAvailability);						
                     }
+					else if (procedure.has('annual_mass/') &&
+							(observedProperty.endsWith('NO23'))) {
+						
+						nitrateDataAvailability.push(dataAvailability);						
+					}	
+					else if (procedure.has('annual_mass/') &&
+							(observedProperty.endsWith('TP'))) {
+						
+						phosphorusDataAvailability.push(dataAvailability);						
+					}	
+					else if (procedure.has('annual_mass/') &&
+							(observedProperty.endsWith('SSC'))) {
+						
+						sedimentDataAvailability.push(dataAvailability);						
+					}
+				});
+		
+				var loadStreamflowTSCollections = [];
+				loadStreamflowTSCollections.push(getTimeSeriesCollection(id, streamflowDataAvailability));
+				var loadStreamflowDataPromises = [];
+				
+				// Retrieve data for each time series collection
+				loadStreamflowTSCollections.forEach(function(tsCollection) {
+					loadStreamflowDataPromises = loadStreamflowDataPromises.concat(tsCollection.retrieveData());
+				});
+				
+				// Sort the data once received and plot.
+				$.when.apply(null, loadStreamflowDataPromises).then(function() {
+					var result = getPlotValues(loadStreamflowTSCollections);
+						
+					var streamflowSeries = {
+						constituentName : nar.Constituents.streamflow.name,
+						constituentUnit : 'Million acre-feet',
+						yearValue : result.current,
+						yearColor : nar.Constituents.streamflow.color,
+						averageName : 'Average 1999-2013',
+						averageValue : result.average
+					};
+
+					var streamflowGraph = ConstituentCurrentYearComparisonPlot(
+							'#barChart1', streamflowSeries);										
+				});
+				
+				var loadNitrateTSCollections = [];
+				loadNitrateTSCollections.push(getTimeSeriesCollection(id, nitrateDataAvailability));
+				var loadNitrateDataPromises = [];
+				
+				// Retrieve data for each time series collection
+				loadNitrateTSCollections.forEach(function(tsCollection) {
+					loadNitrateDataPromises = loadNitrateDataPromises.concat(tsCollection.retrieveData());
+				});
+				
+				// Sort the data once received and plot.
+				$.when.apply(null, loadNitrateDataPromises).then(function() {
+					var result = getPlotValues(loadNitrateTSCollections);
+						
+					var nitrateSeries = {
+						constituentName : nar.Constituents.nitrate.name,
+						constituentUnit : 'Million Tons',
+						yearValue : result.current,
+						yearColor : nar.Constituents.nitrate.color,
+						averageName : 'Average 1991-2013',
+						averageValue : result.average
+					};
+
+					var nitrateGraph = ConstituentCurrentYearComparisonPlot(
+							'#barChart2', nitrateSeries);
+				});
+				
+				var loadPhosphorusTSCollections = [];
+				loadPhosphorusTSCollections.push(getTimeSeriesCollection(id, phosphorusDataAvailability));
+				var loadPhosphorusDataPromises = [];
+				
+				// Retrieve data for each time series collection
+				loadPhosphorusTSCollections.forEach(function(tsCollection) {
+					loadPhosphorusDataPromises = loadPhosphorusDataPromises.concat(tsCollection.retrieveData());
+				});
+				
+				// Sort the data once received and plot.
+				$.when.apply(null, loadPhosphorusDataPromises).then(function() {
+					var result = getPlotValues(loadPhosphorusTSCollections);
+					
+					var phosphorusSeries = {
+						constituentName : nar.Constituents.phosphorus.name,
+						constituentUnit : 'Million Tons',
+						yearValue : result.current,
+						yearColor : nar.Constituents.phosphorus.color,
+						averageName : 'Average 1985-2013',
+						averageValue : result.average
+					};
+
+					var phosphorusGraph = ConstituentCurrentYearComparisonPlot(
+							'#barChart3', phosphorusSeries);
+				});
+				
+				var loadSedimentTSCollections = [];
+				loadSedimentTSCollections.push(getTimeSeriesCollection(id, sedimentDataAvailability));
+				var loadSedimentDataPromises = [];
+				
+				// Retrieve data for each time series collection
+				loadSedimentTSCollections.forEach(function(tsCollection) {
+					loadSedimentDataPromises = loadSedimentDataPromises.concat(tsCollection.retrieveData());
+				});
+				
+				// Sort the data once received and plot.
+				$.when.apply(null, loadSedimentDataPromises).then(function() {
+					var result = getPlotValues(loadSedimentTSCollections);
+					
+					var sedimentSeries = {
+						constituentName : nar.Constituents.sediment.name,
+						constituentUnit : 'Million Tons',
+						yearValue : result.current,
+						yearColor : nar.Constituents.sediment.color,
+						averageName : 'Average 1990-2013',
+						averageValue : result.average
+					};
+
+					var sedimentGraph = ConstituentCurrentYearComparisonPlot(
+							'#barChart4', sedimentSeries);			
 				});
 			};
 			var failedGetDataAvailability = function(data, textStatus,jqXHR) {
-			var msg = 'Could not determine data availability for this site';
+				var msg = 'Could not determine data availability for this site';
 				// Errors are caught by window and alert is displayed
 				throw Error(msg);
 			};
 
 			$.when(getDataAvailability).then(
 				successfulGetDataAvailability,
-				failedGetDataAvailability);
+				failedGetDataAvailability);	
 		});
 	});
