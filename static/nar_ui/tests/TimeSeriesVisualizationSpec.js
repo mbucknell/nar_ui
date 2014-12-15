04e5552b495488188c8d393ae57915cc37afba08
Tomcat Application Server <tomcat@cida-eros-nardev.er.usgs.gov>
2014-12-15 09:57:34 -0600

    automatic inclusion of the env and static files

---
+++ static/nar_ui/tests/TimeSeriesVisualizationSpec.js
@@ -0,0 +1,32 @@
+describe('nar.timeSeries.Visualization, nar.timeSeries.VisualizationRegistry', function(){
+	
+	describe('getComponentsOfId', function(){
+		var getComponentsOfId = nar.timeSeries.Visualization.getComponentsOfId;
+		var newTsvRegistry = new nar.timeSeries.VisualizationRegistry();
+		var createId = newTsvRegistry.getTimeSeriesVisualizationId;
+		var observablePropertyId = 'TP';
+		var timestepDensity = 'annual'
+		var procedureCategory = 'concentration';
+		var procedureSubcategory = 'flow_weighted';
+		var simpleProcedureId = [timestepDensity, procedureCategory].join('_');
+		var complicatedProcedureId = [timestepDensity, procedureCategory, procedureSubcategory].join('_');
+		var simpleTsvId = createId(observablePropertyId, simpleProcedureId);
+		var complicatedTsvId = createId(observablePropertyId, complicatedProcedureId);
+		
+		it('parses correct components from hierarchical ids with a procedure category and NO procedure subcategory', function(){		
+			var components = getComponentsOfId(simpleTsvId);
+			expect(components.constituent).toBe('phosphorus');
+			expect(components.category).toBe(procedureCategory);
+			expect(components.subcategory).toBeUndefined();
+			expect(components.timestepDensity).toBe(timestepDensity);
+		});
+		
+		it('parses correct components from hierarchical ids with a procedure category, a procedure subcategory', function(){
+			var components = getComponentsOfId(complicatedTsvId);
+			expect(components.constituent).toBe('phosphorus');
+			expect(components.category).toBe(procedureCategory);
+			expect(components.subcategory).toBe(procedureSubcategory);
+			expect(components.timestepDensity).toBe(timestepDensity);
+		});
+	});
+});
---
+++ static/nar_ui/time_series/TimeSeriesVisualizationRegistry.js
@@ -0,0 +1,130 @@
+//@requires nar.timeSeries.TimeSeriesVisualization
+var nar = nar || {};
+
+(function(){
+
+nar.timeSeries = nar.timeSeries || {};
+
+/**
+ * @class
+ * In the application, this should be used as a singleton by constructing a single instance
+ * from the onready/main file.
+ * 
+ * In testing this can be used in non-singleton form.
+ * 
+ * While the first instantiation of this class proceeds without further ado,
+ * a warning is printed during each subsequent instantiation.
+ */
+
+//private class variable
+var alreadyBeenInstantiated = false;
+
+nar.timeSeries.VisualizationRegistry = function(){
+    if(alreadyBeenInstantiated){
+    	console.warn(
+			'This object should be treated as a singleton unless testing.' + 
+			'You are instatiating an object that is meant to be used as a singleton, and it has already been instantiated.'+
+    		'Did you mean "nar.timeSeries.VisualizationRegistryInstance" ?.'
+		);
+    }
+    else{
+    	alreadyBeenInstantiated = true;
+    }
+	var self = this;
+    
+    //registry entries, a map of string ids to instantiations of TimeSeriesVisualizations
+    var entries = {};
+    
+    /**
+     * Get the Visualization for a given id if it has already been registered. Return undefined if no such id is registered.
+     * @param {string} id - the Visualization id
+     * @returns {nar.timeSeries.Visualization|undefined} - undefined if not present 
+     */
+    self.get = function(id){
+        var existingTimeSeriesViz = entries[id]; 
+        return existingTimeSeriesViz;
+    };
+    /**
+     * @param {nar.timeSeries.Visualization} timeSeriesVisualization
+     * @throws Error if already registered 
+     */
+    self.register = function(timeSeriesVisualization){
+        var id = timeSeriesVisualization.id;
+        var existing = self.get(id);
+        if(existing){
+            throw Error('A Time Series Visualization of id "' + id + '" already exists');
+        }
+        else{
+            entries[id] = timeSeriesVisualization;
+        }
+    };
+    /**
+     * Return all registered time series visualizations
+     * @returns {Array<nar.timeSeries.Visualization>}
+     */
+    self.getAll = function(){
+      return Object.values(entries);
+    };
+    /**
+     * Convenience method
+     * @param {String} observedProperty
+     * @returns {nar.timeSeries.Visualization
+     */
+    self.getByObservedPropertyAndProcedure = function(observedProperty, procedure){
+        var vizId = self.getTimeSeriesVisualizationId(observedProperty, procedure);
+        var viz = self.get(vizId);
+        return viz;
+    };
+    
+    
+    
+    var strippedObservedPropertyToVizIdMap = {
+            //empty for now
+    };
+    self.urlPrefix = CONFIG.sosDefsBaseUrl;
+    self.stripUrlPrefix = function(url, urlPrefix){
+        return url.replace(urlPrefix, '');
+    };
+    /**
+     * Some Visualizations have just one TimeSeries. In this case, the Visualization id is the observedProperty of the TimeSeries 
+     * minus self.urlPrefix.
+     * Other TVisualizations have multiple TimeSeries. In that case, the Visualization id is be a string representative of 
+     * the visualized time series. 
+     * @param {string} observedProperty - the full uri for the SOS observedProperty
+     * @param {string} procedure - the full uri for the SOS procedure
+     * @returns {string} visualization id
+     *  
+     */
+    self.getTimeSeriesVisualizationId= function(observedProperty, procedure){
+        var strippedObservedProperty = self.stripUrlPrefix(observedProperty, self.urlPrefix + 'property/');
+        //@todo check striped property id to see if it corresponds to a category
+        //where multiple time series correspond to a single visualization
+               
+        //@todo failing id lookup by category, also check strippedObservedPropertyToVizIdMap to see if  
+        //it maps to an id
+        
+        var strippedProcedure = self.stripUrlPrefix(procedure, self.urlPrefix + 'procedure/');
+        
+        //The sos procedure names are set up to contain underscore-delimeted tokens. The first token is timestep density,
+        //one of (discrete, annual, monthly, daily). The second token is category, one of (flow, concentration).
+        //The third token, if present, is a subcategory, one of (mass_L95/*, mass_U95/*, flow_weighted/*, mean/*)
+        //where '*' denotes any modtype.
+        var targetDelim = '/';
+        var properlyDelimetedProcedure = strippedProcedure.replace('_', targetDelim).replace('_', targetDelim);
+        var splitProcedure = properlyDelimetedProcedure.split(targetDelim);
+        //if procedure has a modtype, remove it -- there is no modtype awareness in TsvIds
+        if(splitProcedure.length === 4){
+        	//fix for using REG_* modtypes (a modtype that contains a '_' delimeter
+        	if(splitProcedure[2] === 'REG'){
+        		properlyDelimetedProcedure = splitProcedure.to(2).join(targetDelim);
+        	}
+        	else{
+        		properlyDelimetedProcedure = splitProcedure.to(splitProcedure.length -1).join(targetDelim);
+        	}
+        }
+        //just return this for now
+        return strippedObservedProperty + '/' + properlyDelimetedProcedure;
+    };
+};
+
+}());
