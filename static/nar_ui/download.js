cc3b0c1614dc2387f29ce7f39a8d914da3a98bf0
Tomcat Application Server <tomcat@cida-eros-nardev.er.usgs.gov>
2014-12-18 13:58:15 -0600

    automatic inclusion of the env and static files

---
+++ static/nar_ui/download.js
@@ -0,0 +1,310 @@
+var nar = nar || {};
+nar.downloads = (function() {
+	"use strict";
+	var pubMembers = {};
+	
+	var NUMBER_OF_YEARS_BACK = 100;
+	var SITE_LAYER_NAME = "NAR:JD_NFSN_sites";
+	var STATION_ID_PROPERTY = "qw_id";
+	var STATION_NAME_PROPERTY = "qw_name";
+	var SITE_TYPE_PROPERTY = "site_type";
+	var STATE_PROPERTY = "state";
+	var MRB_VALUE = "Mississippi River Basin";
+	var MS_SITE_ATTR_NAME = 'mssite';
+	var MS_SITE_VALUE = 'MS';
+	var STATE_LIST = {
+		"AL": "Alabama",
+		"AK": "Alaska",
+		"AS": "American Samoa",
+		"AZ": "Arizona",
+		"AR": "Arkansas",
+		"CA": "California",
+		"CO": "Colorado",
+		"CT": "Connecticut",
+		"DE": "Delaware",
+		"DC": "District Of Columbia",
+		"FM": "Federated States Of Micronesia",
+		"FL": "Florida",
+		"GA": "Georgia",
+		"GU": "Guam",
+		"HI": "Hawaii",
+		"ID": "Idaho",
+		"IL": "Illinois",
+		"IN": "Indiana",
+		"IA": "Iowa",
+		"KS": "Kansas",
+		"KY": "Kentucky",
+		"LA": "Louisiana",
+		"ME": "Maine",
+		"MH": "Marshall Islands",
+		"MD": "Maryland",
+		"MA": "Massachusetts",
+		"MI": "Michigan",
+		"MN": "Minnesota",
+		"MS": "Mississippi",
+		"MO": "Missouri",
+		"MT": "Montana",
+		"NE": "Nebraska",
+		"NV": "Nevada",
+		"NH": "New Hampshire",
+		"NJ": "New Jersey",
+		"NM": "New Mexico",
+		"NY": "New York",
+		"NC": "North Carolina",
+		"ND": "North Dakota",
+		"MP": "Northern Mariana Islands",
+		"OH": "Ohio",
+		"OK": "Oklahoma",
+		"OR": "Oregon",
+		"PW": "Palau",
+		"PA": "Pennsylvania",
+		"PR": "Puerto Rico",
+		"RI": "Rhode Island",
+		"SC": "South Carolina",
+		"SD": "South Dakota",
+		"TN": "Tennessee",
+		"TX": "Texas",
+		"UT": "Utah",
+		"VT": "Vermont",
+		"VI": "Virgin Islands",
+		"VA": "Virginia",
+		"WA": "Washington",
+		"WV": "West Virginia",
+		"WI": "Wisconsin",
+		"WY": "Wyoming"
+	};
+	
+	pubMembers.updateSelect2Options = function(select2El, values, placeHolderText, displayKey) {
+		var previousValues = select2El.select2('val');
+		select2El.find('option') .remove();
+		for(var val in values) {
+			var opt = $('<option>');
+			opt.attr('value', val);
+			if(displayKey) {
+				opt.html(val + " - " + values[val]);
+			} else {
+				opt.html(values[val]);
+			}
+			select2El.append(opt);
+		}
+		select2El.select2('destroy');
+		select2El.select2({
+			placeholder: placeHolderText,
+			allowClear: true
+		});
+		select2El.select2('val', previousValues).trigger("change");
+	};
+	
+	pubMembers.getFilteredSiteTypeOptions = function(stationData, selectedStates){
+		//loop through features collecting  site types
+		var siteTypes = {};
+		
+		var mrbIncluded = false;
+		for(var i = 0; i < stationData.features.length; i++) {
+			var props = stationData.features[i].properties;
+			//only add to values object if property hasn't already been added
+			if(!selectedStates || selectedStates.length <= 0 || selectedStates.some(props[STATE_PROPERTY])) { 
+				if(!siteTypes[props[SITE_TYPE_PROPERTY]]){ //only add station if it is flagged as MS
+					siteTypes[props[SITE_TYPE_PROPERTY]] = props[SITE_TYPE_PROPERTY];
+				}
+				
+				if(props[MS_SITE_ATTR_NAME] === MS_SITE_VALUE) {
+					mrbIncluded = true
+				}
+			}
+		}
+		
+		//Add in MRB, a special case not part of the database
+		if(!selectedStates || selectedStates.length <= 0 || mrbIncluded) {
+			siteTypes[MRB_VALUE] = MRB_VALUE;
+		}
+		
+		return siteTypes;
+	};
+	
+	pubMembers.getFilteredStationIdsOptions = function(stationData, selectedStates, selectedSiteTypes) {
+		//loop through features collecting stations
+		var stations = {};
+		
+		var mrbSelected = false; //used to detect if we have to filter down to Mississipi sites
+		if(selectedSiteTypes && selectedSiteTypes.some(MRB_VALUE)) {
+			mrbSelected = true;
+			selectedSiteTypes.remove(MRB_VALUE); //remove so stations are not expected to have type MRB, which doesn't exist as a real site type
+		}
+		for(var i = 0; i < stationData.features.length; i++) {
+			var props = stationData.features[i].properties;
+			if(!stations[props[STATION_ID_PROPERTY]] //only add to values object if property hasn't already been added
+				&& (!selectedStates || selectedStates.length <= 0 || selectedStates.some(props[STATE_PROPERTY])) //only add if has state prop in state list (if state list exists)
+				&& (!selectedSiteTypes || selectedSiteTypes.length <= 0 || selectedSiteTypes.some(props[SITE_TYPE_PROPERTY]))//only add if site type prop in site type list (if site type list exists)
+				) {
+				if(mrbSelected) {
+					//only add station if it is flagged as MS
+					if(props[MS_SITE_ATTR_NAME] === MS_SITE_VALUE) {
+						stations[props[STATION_ID_PROPERTY]] = props[STATION_NAME_PROPERTY];
+					}
+				} else {
+					stations[props[STATION_ID_PROPERTY]] = props[STATION_NAME_PROPERTY];
+				}
+			}
+		}
+		
+		return stations;
+	};
+	
+	// Utility function to enable/disable elements
+	pubMembers.toggleElement = function($el, enable) {
+		if (enable) {
+			$el.removeAttr('disabled');
+		} else {
+			$el.attr('disabled', 'disabled');
+		}
+	};
+	
+	pubMembers.toggleDownloadButton = function(){
+		pubMembers.toggleElement($('#download-button'), $('input[name="dataType"]').is(':checked'));
+	};
+	
+	pubMembers.toggleWaterQuality = function() {
+		var on = $('#waterQuality').is(':checked');
+		var $constituent = $('#constituent');
+		var $qwDataType = $('#qwDataType');
+		
+		pubMembers.toggleElement($constituent, on);
+		pubMembers.toggleElement($qwDataType, on);
+		if (!on) {
+			$constituent.select2('val', []);
+			$qwDataType.select2('val', []);
+		}
+	};
+	
+	pubMembers.toggleStreamFlow = function() {
+		var on = $('#streamFlow').is(':checked');
+		var $streamFlow = $('#streamFlowType');
+		
+		pubMembers.toggleElement($streamFlow, on);
+		if (!on) {
+			$streamFlow.select2('val', []);
+		}
+	};
+	
+	pubMembers.filterSiteTypesByState = function(stationData, selectedStates) {
+		pubMembers.updateSelect2Options(
+				$("#siteType"), 
+				pubMembers.getFilteredSiteTypeOptions(stationData, selectedStates), 
+				"Select a Site Type (optional)",
+				false
+				);
+	};
+	
+	pubMembers.filterStationsByStateAndType = function(stationData, selectedStates, selectedSiteTypes) {
+		pubMembers.updateSelect2Options(
+				$("#stationId"), 
+				pubMembers.getFilteredStationIdsOptions(stationData, selectedStates, selectedSiteTypes), 
+				"Select a Station (optional)",
+				true
+				);
+	};
+	
+	pubMembers.loadAndRenderSiteFilters = function() {
+		var STATION_DATA;
+		$.ajax({
+			url: CONFIG.endpoint.geoserver +
+			"ows?service=WFS&version=1.0.0&request=GetFeature&typeName=" + SITE_LAYER_NAME + 
+			"&outputFormat=json",
+			async: false,
+			dataType: "json",
+			success: function(data) {
+				STATION_DATA = data; //save to singleton
+				
+				//get initial drop down states to render
+				var selectedStates = $("#state").select2('val');
+				var selectedSiteTypes = $("#siteType").select2('val');
+				pubMembers.filterSiteTypesByState(STATION_DATA, selectedStates);
+				pubMembers.filterStationsByStateAndType(STATION_DATA, selectedStates, $("#siteType").select2('val'));
+				
+				//wire filters
+				$("#state").on('change', function() {
+					var selectedStates = $("#state").select2('val');
+					pubMembers.filterSiteTypesByState(STATION_DATA, selectedStates);
+					pubMembers.filterStationsByStateAndType(STATION_DATA, selectedStates, $("#siteType").select2('val'));
+				});
+				$("#siteType").on('change', function() {
+					pubMembers.filterStationsByStateAndType(STATION_DATA, $("#state").select2('val'), $("#siteType").select2('val'));
+				});
+			},
+			context: this
+		});
+	};
+	
+	pubMembers.initDownloadPage = function() {
+		var defaultDateLimits = {
+			minDate : "-" + NUMBER_OF_YEARS_BACK + "Y",
+			maxDate: "+0D",
+		};
+		
+		var datepickerOptions = defaultDateLimits;
+		$.extend(datepickerOptions, {
+			yearRange: "-" + NUMBER_OF_YEARS_BACK + "Y:+0D",
+			changeMonth: true,
+			changeYear: true
+		});
+		
+		$("#startDateTime").datepicker(datepickerOptions);
+		$("#endDateTime").datepicker(datepickerOptions);
+		
+		$("#startDateTime").change(function() {
+			$('#endDateTime').datepicker('option', 'minDate', $(this).datepicker('getDate'));
+		});
+		$('#endDateTime').change(function() {
+			$('#startDateTime').datepicker('option', 'maxDate', $(this).datepicker('getDate'));
+		});
+		
+		//populate html, then init select 2
+		pubMembers.updateSelect2Options($("#state"), STATE_LIST, "Select a State (optional)", true);
+		
+		//will hold a one time fetch of all station data
+		pubMembers.loadAndRenderSiteFilters();
+	
+		$("#constituent").select2({
+			placeholder: "Select a Constituent (optional)",
+			allowClear: true
+		});
+	
+		$("#qwDataType").select2({
+			placeholder: "Select a Water Quality Data Type (optional)",
+			allowClear: true
+		});
+	
+		$('#streamFlowType').select2({
+			placeholder : 'Select a Stream Flow Time Series',
+			allowClear : true
+		});
+	
+		$('input[name="dataType"]').on('click', pubMembers.toggleDownloadButton);
+		pubMembers.toggleDownloadButton();
+		
+		$('#waterQuality').on('click', pubMembers.toggleWaterQuality);
+		pubMembers.toggleWaterQuality();
+		
+		$('#streamFlow').on('click', pubMembers.toggleStreamFlow);	
+		pubMembers.toggleStreamFlow();
+		
+		$('#clear-filters-button').on('click', function () {
+			$('.select2-container').select2('val','');
+			$('input[type="checkbox"]').prop('checked', false);
+			$('.hasDatepicker').val('');
+			$('.hasDatepicker').datepicker('option', defaultDateLimits);
+			pubMembers.toggleDownloadButton();
+			pubMembers.toggleWaterQuality();
+			pubMembers.toggleStreamFlow();
+		});
+			
+		$('#download-button').on('click', function (event) {
+			event.preventDefault();
+			var url = CONFIG.endpoint.nar_webservice + "download/bundle/zip?" + $('#downloadForm').serialize();
+			window.open(url, 'Download');
+		});
+	};
+	
+	return pubMembers;
+}());
---
+++ static/nar_ui/plots/Hydrograph.js
@@ -0,0 +1,297 @@
+var nar = nar || {};
+nar.plots = nar.plots || {};
+(function() {
+
+	var FLOW_DATA_CLASS = 'flow_data_inner_plot';
+	var HYDROGRAPH_ID = 'hydrograph';
+	var FLOW_DURATION_ID = 'flowDuration';
+	
+	var sharedFlowData;
+	
+	nar.plots.FlowWrapper = function(tsViz) {
+		var plotContainer = tsViz.plotContainer;
+		var displayedPlots = 
+			[
+				nar.plots.Hydrograph(tsViz),
+				nar.plots.FlowDuration(tsViz)
+			];
+		var remove = function() {
+			displayedPlots.each(function(plot) {
+				plot.remove();
+			});
+		};
+		var shutdown = function() {
+			displayedPlots.each(function(plot) {
+				plot.shutdown();
+			});
+		};
+		var getOptions = function() {
+			// no modifying options from outside
+			return undefined;
+		};
+		var setupGrid = function() {
+			displayedPlots.each(function(plot) {
+				plot.setupGrid();
+			});
+		};
+		var draw = function() {
+			displayedPlots.each(function(plot) {
+				plot.draw();
+			});
+		};
+		return {
+			getOptions : getOptions,
+			setupGrid : setupGrid,
+			draw : draw,
+			remove : remove,
+			shutdown : shutdown
+		};
+	};
+
+	var LEGEND_PADDING = 5,
+	LEGEND_SPACE_BETWEEN_SYMBOL_AND_LABEL = 5,
+	LEGEND_SYMBOL_WIDTH = 5;
+	var symbolEntryRenderer = function(legendCtx, thisSeries, options, entryOriginX, entryOriginY, fontOptions, maxEntryWidth, maxEntryHeight){
+		var textHeight = legendCtx.measureText('M').width;
+		
+		var symbolX = entryOriginX + LEGEND_PADDING,
+			symbolY = entryOriginY + LEGEND_PADDING + 0.6*textHeight;//it must be offset by a little more than half of the text height to appear aligned
+		
+		var symbolRenderer = thisSeries.points.symbol;
+		var originalFillStyle = legendCtx.fillStyle;
+		legendCtx.fillStyle = thisSeries.color;
+		symbolRenderer(legendCtx, symbolX, symbolY, thisSeries.points.radius);
+		legendCtx.fill();
+		var textX = symbolX + LEGEND_SYMBOL_WIDTH + LEGEND_SPACE_BETWEEN_SYMBOL_AND_LABEL,
+			textY = entryOriginY + LEGEND_PADDING + textHeight; 
+		legendCtx.fillText(thisSeries.label, textX, textY);
+		legendCtx.fillStyle = originalFillStyle;
+	};
+	var roughSymbolLegendSize = {height: 15, width: 125};
+	var canvasLegendOptions = {
+		show : true,
+		entrySize : roughSymbolLegendSize,
+		entryRender : symbolEntryRenderer,
+		layout : $.plot.canvasLegend.layouts.horizontal,
+		backgroundOpacity : 0,
+		position : 'ne'
+	};
+	/**
+	 * @param {TimeSeriesVisualization}
+	 *            tsViz returns {jqPlot}
+	 */
+	nar.plots.Hydrograph = function(tsViz) {
+		
+		var plot;
+		var plotContainer = tsViz.plotContainer;
+		var hydrographDiv = $('#' + HYDROGRAPH_ID);
+		if (hydrographDiv.length == 0) {
+			hydrographDiv = $('<div>').attr('id', HYDROGRAPH_ID).addClass(FLOW_DATA_CLASS);
+			plotContainer.append(hydrographDiv);
+		}
+		sharedFlowData = sharedFlowData || tsViz.timeSeriesCollection.getData();
+		// get the last x value from hydrograph data as year
+		var waterYear = Date.create(sharedFlowData[0].last()[0]).getFullYear();
+		
+		var flowSeries = {
+			data: sharedFlowData[0],
+			color: 'black',
+			lines: {
+				show: true,
+				fill: true,
+				lineWidth: 1,
+				fillColor: 'lightgrey'
+			}
+		};
+		
+		var sampleDates = nar.plots.PlotUtils.createPinnedPointData(sharedFlowData[1], sharedFlowData[0]);
+		
+		var tnSeries = {
+			label: 'Water-quality sample',
+			data: sampleDates,
+			color: 'red',
+			points: {
+				show: true,
+				fill: true,
+				fillColor: 'red',
+				radius: 3,
+				symbol: 'triangle'
+			}
+		};
+
+		plot = $.plot(hydrographDiv, [ flowSeries, tnSeries ], {
+			canvas : true,
+			xaxis : {
+				axisLabel : waterYear,
+				axisLabelUseCanvas : true,
+				axisLabelFontSizePixels : 12,
+				axisLabelFontFamily : "Verdana, Arial, Helvetica, Tahoma, sans-serif",
+				axisLabelPadding : 5,
+				mode : 'time',
+				timeformat : "%b",
+				tickLength : 10,
+				minTickSize : [ 1, 'month' ]
+			},
+			yaxis : {
+				axisLabel : 'Daily mean streamflow, in cubic feet per second',
+				axisLabelUseCanvas : true,
+				axisLabelFontSizePixels : 10,
+				axisLabelFontFamily : "Verdana, Arial, Helvetica, Tahoma, sans-serif",
+				axisLabelPadding : 5,
+				tickLength : 10
+			},
+			grid : {
+				hoverable : true,
+				autoHighlight : true
+			},
+			legend : {
+				show : false,
+			},
+			canvasLegend: canvasLegendOptions
+		});
+		var hoverFormatter = nar.plots.PlotUtils.utcDatePlotHoverFormatter;
+		nar.plots.PlotUtils.setPlotHoverFormatter(hydrographDiv, hoverFormatter);
+		
+		return plot;
+	};
+	
+	nar.plots.FlowDuration = function(tsViz) {
+		var plot;
+		var plotContainer = tsViz.plotContainer;
+		var flowDurationDiv = $('#' + FLOW_DURATION_ID);
+		if (flowDurationDiv.length == 0) {
+			flowDurationDiv = $('<div>').attr('id', FLOW_DURATION_ID).addClass(FLOW_DATA_CLASS);
+			plotContainer.append(flowDurationDiv);
+		}
+		
+		sharedFlowData = sharedFlowData || tsViz.timeSeriesCollection.getData();
+		
+		var sortedFlowData = sharedFlowData[0].sortBy(function(point) {
+			return point[1];
+		}, true);
+		
+		var maxFlow = sortedFlowData.first()[1];
+		var minFlow = sortedFlowData.last()[1];
+		
+		var exceedanceWithDates = sortedFlowData.map(function(point, index) {
+			var exceedance = 100 * ((index + 1) / sortedFlowData.length);
+			return [exceedance, point[1], point[0]];
+		});
+		
+		var swappedSampleDates = sharedFlowData[1].map(function(point) {
+			return [nar.plots.PlotUtils.findNearestYValueAtX(exceedanceWithDates, point[0], 2, 0), point[1]];
+		});
+		
+		var exceedanceDatesRemoved = exceedanceWithDates.map(function(point) {
+			return [point[0], point[1]];
+		});
+		
+		var sampleDates = nar.plots.PlotUtils.createPinnedPointData(swappedSampleDates, exceedanceDatesRemoved);
+		
+		var flowSeries = {
+			data: exceedanceDatesRemoved,
+			color: 'black',
+			lines: {
+				show: true,
+				lineWidth: 1
+			}
+		};
+
+		var sampleSeries = {
+			label: 'Water-quality sample',
+			data: sampleDates,
+			color: 'red',
+			points: {
+				show: true,
+				fill: true,
+				fillColor: 'red',
+				radius: 3,
+				symbol: 'triangle'
+			}
+		};
+		
+		plot = $.plot(flowDurationDiv, [ flowSeries, sampleSeries ], {
+			canvas : true,
+			xaxis : {
+				axisLabel : 'Percent of time exceeded',
+				axisLabelUseCanvas : true,
+				axisLabelFontSizePixels : 12,
+				axisLabelFontFamily : "Verdana, Arial, Helvetica, Tahoma, sans-serif",
+				axisLabelPadding : 5,
+				ticks: [[.01,.01],
+					[.1,''],
+					[1,''],
+					[5,''],
+					[10,10],
+					[20,20],
+					[30,30],
+					[40,40],
+					[50,50],
+					[60,60],
+					[70,70],
+					[80,80],
+					[90,90],
+					[95,''],
+					[99,99],
+					[99.9,''],
+					[99.99,'']],
+				tickLength : 10,
+				min: 0,
+				max: 100
+			},
+			yaxis : {
+				transform : function(y) {
+					return Math.log(y);
+				},
+				inverseTransform : function(y) {
+					return Math.exp(y);
+				},
+				axisLabel : 'Daily mean streamflow, in cubic feet per second',
+				axisLabelUseCanvas : true,
+				axisLabelFontSizePixels : 10,
+				axisLabelFontFamily : "Verdana, Arial, Helvetica, Tahoma, sans-serif",
+				axisLabelPadding : 5,
+				tickLength : 10,
+				ticks : function(axis) {
+					var result = [];
+					var tick
+					
+					if (axis.min > 0.0) {
+						tick = axis.min;
+					}
+					else {
+						// set it to something other zero to prevent infinite loop
+						tick = 0.01;
+					}
+					
+					while (tick <= axis.max) {
+						if (tick < 1) {
+							result.push([tick, tick.toString()]); // This assumes we don't have values less than 0.1
+						}
+						else {
+							result.push([tick, tick.format()]);
+						}
+						tick = tick * 10.0;
+					}
+					return result;
+				},
+				
+				min: nar.plots.PlotUtils.getLogAxisForValue(minFlow, Math.floor),
+				max: nar.plots.PlotUtils.getLogAxisForValue(maxFlow, Math.ceil),
+			},
+			grid : {
+				hoverable : true,
+				autoHighlight : true
+			},
+			legend : {
+				show : false
+			},
+			canvasLegend: canvasLegendOptions
+		});
+		var hoverFormatter = function(x, y) {
+			return x.toFixed(2) + '% - ' + y 
+		};
+		nar.plots.PlotUtils.setPlotHoverFormatter(flowDurationDiv, hoverFormatter);
+		return plot;
+	};
+})();
