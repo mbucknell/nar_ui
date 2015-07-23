d3973d2504f2fa02224a30f901fa87838a7ecdb7
Tomcat Application Server <tomcat@cida-eros-nardev.er.usgs.gov>
2015-07-23 13:41:20 -0500

    automatic inclusion of the env and static files

---
+++ static/nar_ui/tests/SubjectSpec.js
@@ -0,0 +1,83 @@
+describe('nar.commons.Subject', function() {
+	CONFIG = {
+			sosDefsBaseUrl : 'http:/test/nar/'
+	};
+	var subject, 
+		observers,
+		numObservers = 3;
+	beforeEach(function(){
+		subject = new nar.commons.Subject();
+		observers = [];
+		(numObservers).times(function(i){
+			var spy = jasmine.createSpy(i);
+			observers.push(spy);
+		});
+	});
+	it('should notify no observers if unobserved', function(){
+		var numCalled = subject.notify();
+		expect(numCalled).toBe(0);
+	});
+	it('should notify all observers if observed', function(){
+		observers.each(function(observer){
+			expect(observer).not.toHaveBeenCalled();
+			subject.observe(observer);
+			expect(observer).not.toHaveBeenCalled();
+		});
+		var numCalled = subject.notify();
+		expect(numCalled).toBe(numObservers);
+		observers.each(function(observer){
+			expect(observer).toHaveBeenCalled();
+		});
+	});
+	it('should be able to pass any kind of argument to observers when notifying', function(){
+		observers.each(function(observer){
+			subject.observe(observer);
+		});
+		
+		//we will call notify with each of these
+		var variousArguments = [
+			[1, 2, 3, []],
+			[1, 'a', false, {}],
+			[new Date(), null, undefined, function(){}]
+		];
+		variousArguments.each(function(argumentSet){
+			subject.notify.apply(null, argumentSet);
+			observers.each(function(observer){
+				var expectObserver = expect(observer);
+				expectObserver.toHaveBeenCalledWith.apply(expectObserver, argumentSet);
+			});
+		});
+	});
+	it('should not call former observers who are no longer observing', function(){
+		//add all
+		observers.each(function(observer){
+			subject.observe(observer);
+		});
+		//remove one
+		var removeMe = observers.first();
+		var wasObserving = subject.unobserve(removeMe);
+		expect(wasObserving).toBe(true);
+		//notify
+		var numberCalled = subject.notify();
+		expect(numberCalled).toBe(numObservers - 1);
+		//verify
+		expect(removeMe).not.toHaveBeenCalled();
+		observers.exclude(removeMe).each(function(observer){
+			expect(observer).toHaveBeenCalled();
+		});
+	});
+	it('should return true if asked to unobserve an observer that is observing', function(){
+		var removeMe = function(){};
+		subject.observe(removeMe);
+		var wasObserving = subject.unobserve(removeMe);
+		expect(wasObserving).toBe(true);
+	});
+	it('should not error, and should return false if asked to unobserve an observer that is not observing', function(){
+		var wasObserving;
+		var test = function(){
+			wasObserving = subject.unobserve(function(){});
+		};
+		expect(test).not.toThrow();
+		expect(wasObserving).toBe(false);
+	});
+});
---
+++ static/nar_ui/time_series/TimeSeriesCollection.js
@@ -0,0 +1,110 @@
+//@requires nar.timeSeries.TimeSeries
+var nar = nar || {};
+nar.timeSeries = nar.timeSeries || {};
+/**
+ * @class
+ */
+nar.timeSeries.Collection = function(){
+    var self = this;
+    var timeSeries = [];
+    
+    //date range is calculated when dirty (after adding new time series)
+    var dirty = true;
+    var cachedTimeRange;
+    
+    /**
+     * A wrapper around sugarjs' map function
+     */
+    self.map = function(customFunction){
+        return timeSeries.map(customFunction);
+    };
+    /**
+     * A wrapper around sugarjs' reduce function
+     */
+    self.reduce = function(customFunction, init){
+    	return timeSeries.reduce(customFunction, init);
+    };
+    /**
+     * Asynchronously retrieves the data for all time series
+     * and passes all time series objects to the success callback.
+     * 
+     * @returns {array<jQuery.Promise>}
+     * @see http://api.jquery.com/jquery.when/
+     * @see https://api.jquery.com/jQuery.deferred/
+     */
+    self.retrieveData = function(){
+        var retrievalPromises = self.map(function(timeSeries){
+            return timeSeries.retrieveData();
+        });
+        return retrievalPromises;
+    };
+    
+    /**
+     * WARNING - Do not add elements to the returned array
+     * If you do, getTimeRange will not behave properly.
+     * To add elements, use self.add
+     */
+    self.getAll = function(){
+        return timeSeries;
+    };
+    /**
+     * @returns {Array<Array>} - Array for data for each TimeSeries within the TimeSeriesCollection
+     */
+    self.getData = function() {
+        var data = self.map(function(timeSeries){
+            return timeSeries.data;
+        });
+        return data;
+    };
+    /**
+     * @returns {Array} - Merges all values from all TimeSeries in the TimeSeriesCollection into one array and returns them sorted on date
+     */
+    self.getDataMerged = function(){
+    	var merged = self.reduce(function(state, current){
+    		return state.concat(current.data);
+    	}, []);
+    	var sorted = merged.sortBy(function(point){
+    		return point[0];
+    	});
+    	return sorted;
+    };
+    /**
+     * 
+     * @param {nar.timeSeries.TimeSeries} aTimeSeries
+     */
+    self.add = function(aTimeSeries){
+        timeSeries.push(aTimeSeries);
+        dirty = true;
+    };
+
+    /**
+     * Returns the time range for a collection of time series.
+     * 
+     * If no time series have been added since the last time range
+     * calculation, it returns the cached time range.
+     * If time series have been added since the last time range
+     * calculation, it calculates the new time range and caches the
+     * result.
+     * 
+     * @returns {nar.timeSeries.TimeRange}
+     */
+    self.getTimeRange = function(){
+        if(dirty && 0 !== timeSeries.length){
+            var timeRanges = timeSeries.map(function(aTimeSeries){return aTimeSeries.timeRange;});
+            cachedTimeRange = nar.timeSeries.TimeRange.ofAll(timeRanges); 
+            dirty=false;
+        }
+        return cachedTimeRange.clone();
+    };
+    
+    /**
+     * @param {string} observedProperty
+     * @returns {nar.timeSeries.TimeSeries}
+     */
+    self.getTimeSeriesByObservedProperty = function(observedProperty){
+        var targetTimeSeries = timeSeries.find(function(potentialTimeSeries){
+            return potentialTimeSeries.observedProperty === observedProperty;
+        });
+        return targetTimeSeries;
+    };
+};
