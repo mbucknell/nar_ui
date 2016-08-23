6b8c431c09f13acb96025862d5b8b192c24051ad
Carl Schroedl <cschroedl@usgs.gov>
2016-08-23 15:03:25 -0500

    Restoring desired functionality.

--- nar_common/static/nar_ui/util.js
+++ nar_common/static/nar_ui/util.js
@@ -193,15 +193,19 @@ nar.util = {};
 			'discrete_concentration' : ['remark', 'concentration']
 	};
 	
-	nar.util.getValueForResponseRow = function(responseRow, procedure){
-		var valueProperties = sosProcedureToValueProperties[procedure];
-		//value can be comprised of multiple fields. Join fields together in order.
-		var value = valueProperties.reduce(function(accumulation, current){
-			//if attribute is missing, use blank string
-			return accumulation + '' + (responseRow[current] || '');
-		}, '');
-		
-		return value;
+    nar.util.getValueForResponseRow = function(responseRow, procedure){
+        var valueProperties = sosProcedureToValueProperties[procedure];
+        //value can be comprised of multiple fields. Join fields together in order.
+        var values = nar.util.concatenatePropertyValues(responseRow, valueProperties);
+        return values;
+    };
+    
+	nar.util.concatenatePropertyValues = function(object, propertyKeys){
+        var values = propertyKeys.reduce(function(accumulation, current){
+            //if attribute is missing, use blank string
+            return accumulation + '' + (object[current] || '');
+        }, '');
+        return values;
 	};
 	
 	nar.util.getTimestampForResponseRow = function(responseRow){
