8aa73d80fa42d997aa4b42727f41ec085402ee58
Tomcat Application Server <tomcat@cida-eros-nardev.er.usgs.gov>
2016-08-24 16:51:30 -0500

    automatic exclusion of the env and static files

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/opt/tomcat/.jenkins/jobs/build_django_app/workspace/env/bin/python2.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
